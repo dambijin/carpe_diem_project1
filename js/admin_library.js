776e48d60b3b79082d65753f4547a6acda953791
임우혁 <dladngur222@naver.com>
2024-02-06 17:28:11 +0900

    자바스크립트 수정 및 파일 삭제

--- js/admin_library.js
+++ js/admin_library.js
@@ -1,22 +1,7 @@
 
-// 회원정보 수정
-function chgInfo() {
-    alert("수정되었습니다");
-    location.href='admin_member_list.html';
-}
-// admin_book_list 등록 팝업창열기
-function bookadd_popup() {
-    window.open
-    ("admin_book_add.html", "책 등록", "width=1000, height=800, left=100, top=100"); 
-}
-// admin_book_add 책 등록           미완성
-function bookAdd() {
-    alert("등록되었습니다");
-    window.close();
-}
 // 닫기
 function closePopup() {
-    window.open('', '_self', '');  // 지원하지 않는 곳 해결
+    window.open('', '_self', '');
     window.close();
 }
 // 도서폐기 전체선택
@@ -29,18 +14,3 @@ function fn_allcheck() {
     }
 }
 
-// 연체상태 popup창
-function overdue_popup() {
-    window.open
-    ("admin_book_overdue.html", "팝업", "width=1000, height=700, left=100, top=100");
-}
-// 연체해제 누르면 확인 후 닫기         미완성
-function overdue_close() {
-    alert("연체해제 되었습니다");
-    window.close();
-}
-// 연체상태 팝업창 닫기
-// function overdue() {
-//     alert("연체해제 되었습니다");
-//     window.close();
-// }
