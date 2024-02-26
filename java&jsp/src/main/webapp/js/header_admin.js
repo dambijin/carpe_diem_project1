4bb6838d630c685495fa0a7c8ac9187162e2819c
임우혁 <dladngur222@naver.com>
2024-02-26 11:58:55 +0900

    admin 자바스크립트 경로 수정

--- java&jsp/src/main/webapp/js/header_admin.js
+++ java&jsp/src/main/webapp/js/header_admin.js
@@ -1,19 +1,19 @@
 document.querySelector('header').innerHTML = `
     <div class="blank_space"> </div>
 
-    <div class="headerbox" onclick="location.href='main.html';">
+    <div class="headerbox" onclick="location.href='main.jsp';">
     <img class="logo" width="30px"
     src="../resource/logo.png">
         도서관리시스템
     </div>
     <div class="sign_up_in">
         <div class="sign_in">
-            <a href="sign_in.html">
+            <a href="sign_in.jsp">
                 로그인&nbsp;
             </a>
         </div>
         <div class="sign_up">
-            <a href="sign_up.html">
+            <a href="sign_up.jsp">
                 회원가입
             </a>
         </div>
@@ -22,9 +22,9 @@ document.querySelector('header').innerHTML = `
 
     <div class="nav">
         <hr class="custom-hr">
-        <div class="flex-header" onclick="location.href='admin_member_list.html';">회원목록</div>
-        <div class="flex-header" onclick="location.href='admin_book_list.html';">재고</div>
-        <div class="flex-header" onclick="location.href='admin_wishbook_list.html';">희망도서목록</div>
+        <div class="flex-header" onclick="location.href='admin_member_list.jsp';">회원목록</div>
+        <div class="flex-header" onclick="location.href='admin_book_list.jsp';">재고</div>
+        <div class="flex-header" onclick="location.href='admin_wishbook_list.jsp';">희망도서목록</div>
         <hr class="custom-hr">
     </div>
 `;
