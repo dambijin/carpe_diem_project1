f116aa279ff7e196312de2179216cb47fcf2ccb1
박상민 <bark22jh@naver.com>
2024-04-16 09:03:41 +0900

    수정중

--- java&jsp/src/main/java/carpedm/mypage/mypage_loan_historyServlet.java
+++ java&jsp/src/main/java/carpedm/mypage/mypage_loan_historyServlet.java
@@ -22,7 +22,8 @@ import javax.sql.DataSource;
 @WebServlet("/mypage_loan_history")
 public class mypage_loan_historyServlet extends HttpServlet {
 
-	
+	int start_page = -1;
+	int end_page = -1;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
@@ -45,41 +46,50 @@ public class mypage_loan_historyServlet extends HttpServlet {
 		ArrayList<Map<String, String>> myInfo = getDBList("select * from member where m_pid = " + login_m_pid);
 		request.setAttribute("myInfo", myInfo);
 		
-		String page = request.getParameter("page");
-		if (page == null || "".equals(page)) {
-			page = "1";
-		}
-		int currentPage = Integer.parseInt(page);
+		// page를 가져옴
+				String page = request.getParameter("page");
+				if (page == null || "".equals(page)) {
+					page = "1";
+				}
+				int currentPage = Integer.parseInt(page);
 
-		// perPage(표시 개수) 처리 부분
-		String perPage = request.getParameter("perPage");
-		if (perPage == null || "".equals(perPage)) {
-			perPage = "10";
-		}
-		int itemsPerPage = Integer.parseInt(perPage);
-		// 페이지 처리를 위한 계산
-		int startRow = (currentPage - 1) * itemsPerPage + 1;
-		int endRow = currentPage * itemsPerPage;
-		request.setAttribute("page", page);
-		request.setAttribute("perPage", perPage);
+				// perPage(표시 개수) 처리 부분
+				String perPage = request.getParameter("perPage");
+				if (perPage == null || "".equals(perPage)) {
+					perPage = "10";
+				}
+				int itemsPerPage = Integer.parseInt(perPage);
+				
+				start_page = ((currentPage - 1) * itemsPerPage) + 1;
+				end_page = currentPage * itemsPerPage;
+				
+				request.setAttribute("page", page);
+				request.setAttribute("perPage", perPage);
 		
-		ArrayList<Map<String, String>> list = getLoan(login_m_pid,search);
+		ArrayList<Map<String, String>> list = getLoanHistory(login_m_pid,search);
 		ArrayList<Map<String, String>> pageList = new ArrayList<>();
 		
-		// 인덱스를 1부터 시작하기 위해 startRow와 endRow를 1씩 감소
-				startRow--;
-				endRow--;
+		String query2 = "";
+		query2 += " select";
+		query2 += " count(*)";
+		query2 += " from";
+		query2 += " reservation";
+		query2 += " inner join book";
+		query2 += " on reservation.b_id = book.b_id";
+		query2 += " inner join library";
+		query2 += " on book.lb_id = library.lb_id";
+		query2 += " LEFT JOIN ";
+		query2 += " loan ON reservation.b_id = loan.b_id";
+		query2 += " where reservation.m_pid = " + login_m_pid ;
+		
+		
+		ArrayList<Map<String, String>> totalList = getDBList(query2);
+		
+		int totalViewCount = Integer.parseInt(totalList.get(0).get("COUNT(*)"));
 
-				for (int i = startRow; i <= endRow; i++) {
-					if (i < list.size()) {
-						pageList.add(list.get(i));
-					} else {
-						break;
-					}
-				}
 
-				request.setAttribute("list", pageList);
-				request.setAttribute("allcount", list.size());
+				request.setAttribute("list", list);
+				request.setAttribute("totalViewCount", totalViewCount);
 		
 	
 //	Gson gson = new Gson();
@@ -103,7 +113,7 @@ public class mypage_loan_historyServlet extends HttpServlet {
 	        return conn;
 	    }
 
-	private ArrayList<Map<String, String>> getLoan(String m_pid, String search) {
+	private ArrayList<Map<String, String>> getLoanHistory(String m_pid, String search) {
 		ArrayList<Map<String, String>> result_list = new ArrayList<Map<String, String>>();
 		try {
 			Connection conn = getConnection();
